218886501e0f74ef8d01b802d5c6db8887bc9ee3
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-09-14 22:42:01 +0200

    feat: create top nav for mobile devices

--- build.js
+++
@@ -1,7 +0,0 @@
-const fs = require(`fs`);
-
-if (fs.existsSync(`dist`)) {
-    fs.rmSync(`dist`, { recursive: true, force: true });
-}
-
-fs.cpSync('src', 'dist', { recursive: true });
---
+++ src/scripts/hamburger.js
@@ -0,0 +1,12 @@
+// HTML Element
+const hamburgersEl = document.getElementsByClassName("hamburger");
+const navigationsEl = document.getElementsByClassName("top-nav");
+
+// Add events
+Array.from(hamburgersEl).forEach(hamburgerEl => {
+    hamburgerEl.addEventListener("click", () => {
+        Array.from(navigationsEl).forEach(navigationEl => {
+            navigationEl.classList.toggle("top-nav-open");
+        });
+    });
+});
