a404693e3b8b954d1cf17e16e47f510741020dfa
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-10-16 20:23:18 +0200

    feat: create navigation logic

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import {Component} from "@angular/core";
+import {NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs";
 
 @Component({
   selector: "repox-website-nav",
@@ -6,6 +8,8 @@ import {Component} from "@angular/core";
   styleUrls: ["./nav.component.css"]
 })
 export class NavComponent {
+  openLink: string = "";
+
   links: Array<{ name: string; href: string; }> = [
     {name: "Home", href: "/home"},
     {name: "Documentation", href: "/documentation"},
@@ -16,9 +20,21 @@ export class NavComponent {
 
   menuIsOpen: boolean = false;
 
-  constructor() {}
+  constructor(private readonly router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => this.openLink = event.url === "/" ? "/home" : event.url);
+  }
 
   toggleMenu(): void {
     this.menuIsOpen = !this.menuIsOpen;
+  }
+
+  linkIsOpen(href: string): boolean {
+    return this.openLink === href;
+  }
+
+  closeMobileMenu(): void {
+    this.menuIsOpen = false;
   }
 }
