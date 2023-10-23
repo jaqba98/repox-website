11fe11e005df3f2eb9d9c190f2954360cfdbdd42
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-10-23 17:13:13 +0200

    feat: create footer component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,18 +4,21 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {RouterLink} from "@angular/router";
 import {MainFeaturesComponent} from "./components/main-features/main-features.component";
+import {FooterComponent} from "./components/footer/footer.component";
 
 @NgModule({
   declarations: [
     NavComponent,
     HeaderComponent,
-    MainFeaturesComponent
+    MainFeaturesComponent,
+    FooterComponent
   ],
   imports: [CommonModule, RouterLink],
   exports: [
     NavComponent,
     HeaderComponent,
-    MainFeaturesComponent
+    MainFeaturesComponent,
+    FooterComponent
   ]
 })
 export class SharedModule {
