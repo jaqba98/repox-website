0da5751fc323e2f2d918a83e89f7f8dc5c499798
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-10-25 14:58:36 +0200

    chore: remove the old folder

--- old/src/scripts/documentation-hamburger.js
+++
@@ -1,8 +0,0 @@
-// HTML Element
-const documentationHamburgerEl = document.getElementById("documentation-hamburger");
-const documentationContainerEl = document.getElementById("documentation-container");
-
-// Add events
-documentationHamburgerEl.addEventListener("click", () => {
-    documentationContainerEl.classList.toggle("hamburger-open");
-});
--- old/src/scripts/top-nav-hamburger.js
+++
@@ -1,8 +0,0 @@
-// HTML Element
-const topNavHamburgerEl = document.getElementById("top-nav-hamburger");
-const topNavContainerEl = document.getElementById("top-nav-container");
-
-// Add events
-topNavHamburgerEl.addEventListener("click", () => {
-    topNavContainerEl.classList.toggle("hamburger-open");
-});
