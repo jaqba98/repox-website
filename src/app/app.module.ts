688192db12ec8d17cb60c1c4d5d155382efa1de5
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-10-26 20:24:07 +0200

    feat: create home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,8 @@ import {CommonModule} from "@angular/common";
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {NavComponent} from "./shared/component/nav/nav.component";
-import {HeaderComponent} from "./shared/component/header/header.component";
-import {MainFeaturesComponent} from "./shared/component/main-features/main-features.component";
-import {StatisticsComponent} from "./shared/component/statistics/statistics.component";
-import {AboutComponent} from "./shared/component/about/about.component";
-import {VideoComponent} from "./shared/component/video/video.component";
-import {TeamComponent} from "./shared/component/team/team.component";
-import {SupportingComponent} from "./shared/component/supporting/supporting.component";
-import {FooterComponent} from "./shared/component/footer/footer.component";
 import {SharedModule} from "./shared/shared.module";
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [
@@ -21,6 +13,7 @@ import {SharedModule} from "./shared/shared.module";
   imports: [
     CommonModule,
     BrowserModule,
+    CoreModule,
     SharedModule
   ],
   providers: [],
