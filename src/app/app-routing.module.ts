e874b4c0001760d14e74f07987655cbd76db62f7
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-10-26 21:04:52 +0200

    feat: create blog and support component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./core/component/home/home.component";
 import {PageNotFoundComponent} from "./core/component/page-not-found/page-not-found.component";
 import {DocsComponent} from "./core/component/docs/docs.component";
+import {SupportComponent} from "./core/component/support/support.component";
+import {BlogComponent} from "./core/component/blog/blog.component";
 
 export const routes: Routes = [
   { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
   { path: "docs", component: DocsComponent },
+  { path: "support", component: SupportComponent },
+  { path: "blog", component: BlogComponent },
   { path: "**", component: PageNotFoundComponent }
 ];
 
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,11 +3,15 @@ import {HomeComponent} from "./component/home/home.component";
 import {SharedModule} from "../shared/shared.module";
 import {PageNotFoundComponent} from "./component/page-not-found/page-not-found.component";
 import {DocsComponent} from "./component/docs/docs.component";
+import {SupportComponent} from "./component/support/support.component";
+import {BlogComponent} from "./component/blog/blog.component";
 
 @NgModule({
   declarations: [
     HomeComponent,
     DocsComponent,
+    SupportComponent,
+    BlogComponent,
     PageNotFoundComponent
   ],
   imports: [
@@ -16,6 +20,8 @@ import {DocsComponent} from "./component/docs/docs.component";
   exports: [
     HomeComponent,
     DocsComponent,
+    SupportComponent,
+    BlogComponent,
     PageNotFoundComponent
   ]
 })
