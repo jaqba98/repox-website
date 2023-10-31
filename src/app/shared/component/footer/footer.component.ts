0fe90a0cb78aff2c1334400b992a9bdfa65946cd
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-10-31 13:58:38 +0100

    feat: create basic footer content

--- src/app/shared/component/footer/footer.component.ts
+++ src/app/shared/component/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import {Component} from "@angular/core";
+import {NavListItemModel} from "../../model/nav-list-item.model";
 
 @Component({
   selector: "repox-website-footer",
@@ -6,4 +7,15 @@ import { Component } from "@angular/core";
   styleUrls: ["./footer.component.css"]
 })
 export class FooterComponent {
+  readonly docsNavTitle: string = "Documentation";
+
+  readonly docsNavListItems: NavListItemModel[] = [
+    {link: "example1", label: "Example 1", children: []},
+    {link: "example2", label: "Example 2", children: []},
+    {link: "example3", label: "Example 3", children: []},
+    {link: "example4", label: "Example 4", children: []},
+    {link: "example5", label: "Example 5", children: []}
+  ];
+
+  readonly docsClass: string = "docs-nav";
 }
