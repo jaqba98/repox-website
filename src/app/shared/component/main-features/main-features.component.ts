91760e67094623c737674b75b9843b6eba46123d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-11-06 14:48:36 +0100

    feat: create main-feature section

--- src/app/shared/component/main-features/main-features.component.ts
+++ src/app/shared/component/main-features/main-features.component.ts
@@ -1,4 +1,6 @@
 import {Component} from "@angular/core";
+import {faCodeBranch, faDiagramProject, faGaugeHigh, faHome} from "@fortawesome/free-solid-svg-icons";
+import {MainFeaturesModel} from "../../model/main-features.model";
 
 @Component({
   selector: "repox-website-main-features",
@@ -6,4 +8,21 @@ import {Component} from "@angular/core";
   styleUrls: ["./main-features.component.css"]
 })
 export class MainFeaturesComponent {
+  readonly mainFeatures: MainFeaturesModel[] = [
+    {
+      title: "Speed",
+      icon: faGaugeHigh,
+      description: "With our tool, increase the efficiency of designing, programming and deploying your projects."
+    },
+    {
+      title: "Single Responsibility",
+      icon: faDiagramProject,
+      description: "With single responsibility, you can achieve more maintainable code that is easy to develop."
+    },
+    {
+      title: "Many projects",
+      icon: faCodeBranch,
+      description: "Store all your projects in one git repository and reuse code between all your applications."
+    }
+  ];
 }
