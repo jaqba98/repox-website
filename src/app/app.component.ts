ba52c65e0de595409f2b2f9f658998ee17ad22c4
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-02-20 08:29:17 +0100

    styles: create styles for main nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import {NavListItemModel} from "./shared/model/nav-list-item.model";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   readonly appNavTitle: string = "Repox";
