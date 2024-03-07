020719440a3a70859bbfcc2bac1602161558d486
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-03-07 22:11:30 +0100

    feat: create terminal component

--- src/app/shared/component/control/control.module.ts
+++ src/app/shared/component/control/control.module.ts
@@ -2,14 +2,16 @@ import {NgModule} from "@angular/core";
 
 import {TerminalComponent} from "./terminal/terminal.component";
 import {PanelModule} from "primeng/panel";
+import {ToastModule} from "primeng/toast";
 
 @NgModule({
   declarations: [
     TerminalComponent
   ],
-  imports: [
-    PanelModule
-  ],
+    imports: [
+        PanelModule,
+        ToastModule
+    ],
   exports: [
     TerminalComponent
   ]
--- src/app/shared/component/control/terminal/terminal.component.ts
+++ src/app/shared/component/control/terminal/terminal.component.ts
@@ -1,10 +1,20 @@
 import {Component, Input} from '@angular/core';
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'repox-website-terminal',
   templateUrl: './terminal.component.html',
-  styleUrl: './terminal.component.scss'
+  styleUrl: './terminal.component.scss',
+  providers: [MessageService]
 })
 export class TerminalComponent {
   @Input() command: string = "";
+
+  constructor(private messageService: MessageService) {
+  }
+
+  onClick(command: string) {
+    navigator.clipboard.writeText(command);
+    this.messageService.add({severity: "success", detail: "Copied"});
+  }
 }
