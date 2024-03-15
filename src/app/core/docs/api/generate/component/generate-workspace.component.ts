1a8e8d43722fe40a0103d41b73c666ad5fbd0091
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-03-15 14:21:36 +0100

    fix: fix the generate workspace content

--- src/app/core/docs/api/generate/component/generate-workspace.component.ts
+++ src/app/core/docs/api/generate/component/generate-workspace.component.ts
@@ -9,35 +9,84 @@ import {DocsContentModel} from "@shared";
 export class GenerateWorkspaceComponent {
   content: DocsContentModel = {
     sections: [
-      {type: "firstLevelHeader", value: "Generate Workspace", link: "generate-workspace"},
-      {type: "newline"},
+      {
+        type: "firstLevelHeader",
+        value: "Generate Workspace",
+        link: "generate-workspace"
+      },
+      {
+        type: "newline"
+      },
+      {
+        type: "secondLevelHeader",
+        value: "Description",
+        link: "description"
+      },
       {
         type: "paragraph",
-        value: "The generate workspace command creates a new workspace within the monorepo project managed by Repox. " +
-          "A workspace serves as a container for projects within the monorepo."
+        value: "The workspace generation section of Repox facilitates the creation of monorepo environments for " +
+          "efficient project management. Users can initiate workspace generation with a simple command, " +
+          "specifying project configurations and dependencies. Repox automates the setup process, creating a " +
+          "unified workspace where multiple projects reside within a single Git repository. This streamlines " +
+          "development workflows, allowing teams to manage dependencies, share code, and enforce consistent " +
+          "standards seamlessly. With Repox, generating and maintaining monorepo workspaces becomes a " +
+          "straightforward task, enhancing collaboration and productivity across the development lifecycle."
       },
-      {type: "newline"},
-      {type: "secondLevelHeader", value: "Syntax", link: "syntax"},
-      {type: "command", value: "repox generate workspace [options]"},
-      {type: "newline"},
-      {type: "secondLevelHeader", value: "Program arguments", link: "program-arguments"},
-      {type: "info", value: "No arguments"},
-      {type: "newline"},
-      {type: "secondLevelHeader", value: "Command arguments", link: "command-arguments"},
+      {
+        type: "newline"
+      },
+      {
+        type: "secondLevelHeader",
+        value: "Syntax",
+        link: "syntax"
+      },
+      {
+        type: "command",
+        value: "repox generate workspace [options]"
+      },
+      {
+        type: "newline"
+      },
+      {
+        type: "secondLevelHeader",
+        value: "Program arguments",
+        link: "program-arguments"
+      },
+      {
+        type: "info",
+        value: "No arguments"
+      },
+      {
+        type: "newline"
+      },
+      {
+        type: "secondLevelHeader",
+        value: "Command arguments",
+        link: "command-arguments"
+      },
       {
         type: "arguments",
         value: [
-          {argument: "name", alias: "n", description: "The name of the workspace.", require: true, value: "string"}
+          {
+            argument: "name",
+            alias: "n",
+            description: "The name of the workspace.",
+            require: true,
+            value: "string"
+          }
         ]
       },
-      {type: "newline"},
-      {type: "secondLevelHeader", value: "Example", link: "example"},
-      {type: "command", value: "repox generate workspace --name=my-workspace"},
-      {type: "newline"},
-      {type: "secondLevelHeader", value: "Note", link: "note"},
       {
-        type: "paragraph",
-        value: "Ensure that the workspace name is unique and adheres to file system naming conventions."
+        type: "newline"
+      },
+      {
+        type: "secondLevelHeader",
+        value: "Example",
+        link: "example"
+      },
+      {
+        type: "command",
+        value: "repox generate workspace --name=my-workspace"
       }
     ]
   };
