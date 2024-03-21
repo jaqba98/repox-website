5b91afc6f26e51d5ed1ee778e27fdb83f53d34c7
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-03-21 09:46:27 +0100

    feat: create installation content

--- src/app/core/docs/getting-started/getting-started-home/getting-started-installation.component.ts
+++ src/app/core/docs/getting-started/getting-started-home/getting-started-installation.component.ts
@@ -13,6 +13,72 @@ export class GettingStartedInstallationComponent {
         type: "firstLevelHeader",
         value: "Installation",
         link: "getting-started"
+      },
+      {
+        type: "newline"
+      },
+      {
+        type: "secondLevelHeader",
+        value: "Prerequisites",
+        link: "prerequisites"
+      },
+      {
+        type: "paragraph",
+        value: "Before installing Repox, ensure you have the following installed:"
+      },
+      {
+        type: "paragraph",
+        value: "- node.js"
+      },
+      {
+        type: "paragraph",
+        value: "- npm"
+      },
+      {
+        type: "newline"
+      },
+      {
+        type: "secondLevelHeader",
+        value: "Installation Steps",
+        link: "installation-steps"
+      },
+      {
+        type: "paragraph",
+        value: "To install Repox globally, follow these steps:"
+      },
+      {
+        type: "paragraph",
+        value: "1. Open your terminal or command prompt."
+      },
+      {
+        type: "paragraph",
+        value: "2. Run the following npm command with `-g` flag to install Repox globally:"
+      },
+      {
+        type: "newline"
+      },
+      {
+        type: "command",
+        value: "npm install -g repox"
+      },
+      {
+        type: "newline"
+      },
+      {
+        type: "secondLevelHeader",
+        value: "Verifing the installation",
+        link: "verifing-the-installation"
+      },
+      {
+        type: "paragraph",
+        value: "To verify ther Repox was installed globally succesfully, you can run the following command to check the installed version:"
+      },
+      {
+        type: "newline"
+      },
+      {
+        type: "command",
+        value: "repox --version"
       }
     ]
   };
